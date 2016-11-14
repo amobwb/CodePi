79c286934f73756fb561efb896cd69dac6693643
amobwb <afshin.mohammadi@solidify.com>
2016-11-14 23:03:17 +0100

    ändrat web.config filer

--- ConsoleApp/Verify/Program.cs
+++ ConsoleApp/Verify/Program.cs
@@ -27,13 +27,33 @@ namespace Verify
         {
             // Create objekt
             Person person1 = new Person();
-            //Print it out
-            Console.WriteLine(person1.text, person1.name);
-            Console.WriteLine(person1.name);
+
+            Console.WriteLine("Please enter your name");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Hello," + name);
+
+            Console.WriteLine("How many hours did you sleep lanst night");
+            int hours = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine(person1.text + person1.name + test);
+            if (hours < 8)
+            {
+                Console.WriteLine("You seems to be tired");
+            }
+            else
+            {
+                Console.WriteLine("you must be happy");
+            }
+            Console.WriteLine("Goodby," + name);
             Console.ReadKey();
 
 
         }
                          
+    }
+    class Account
+    {
+
     }
 }
